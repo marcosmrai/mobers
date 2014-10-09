f09a1a04345a9877b53a9185d18943683e6f9067
thalitadru <thalitafdrumond@gmail.com>
2014-10-09 11:48:24 -0300

    Added function to read Movielens DB into expected format

--- dbread.py
+++ dbread.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 def fake_ratings(noise=.25):
     u = []
@@ -12,19 +12,29 @@ def fake_ratings(noise=.25):
     
     # Generate the latent user and item vectors
     for i in range(num_users):
-        u.append(2 * numpy.random.randn(latent_dimension))
+        u.append(2 * np.random.randn(latent_dimension))
     for i in range(num_items):
-        v.append(2 * numpy.random.randn(latent_dimension))
+        v.append(2 * np.random.randn(latent_dimension))
         
     # Get num_ratings ratings per user.
     for i in range(num_users):
-        items_rated = numpy.random.permutation(num_items)[:num_ratings]
+        items_rated = np.random.permutation(num_items)[:num_ratings]
 
         for jj in range(num_ratings):
             j = items_rated[jj]
-            rating = numpy.sum(u[i] * v[j]) + noise * numpy.random.randn()
+            rating = np.sum(u[i] * v[j]) + noise * np.random.randn()
         
             ratings.append((i, j, rating))  # thanks sunquiang
 
     return (ratings, u, v)
 
+def read_ratings(filename):
+    with open(filename, 'r') as f:
+        # base is in format: user_ID  item_ID  rating (tab separated)
+        ratings = [tuple([int(elem) for elem in line.split('\t')[0:-1]]) \
+                   for line in f]
+    return ratings
+            
+            
+if __name__=='__main__':
+    print read_ratings('ml-100k/u.data')
