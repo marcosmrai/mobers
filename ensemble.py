c8971b87abe043b17b0d6c0e5e7e56e5f040baf8
thalitadru <thalitafdrumond@gmail.com>
2014-11-19 19:35:22 -0200

    Reorganized recommend and ensemble
    Reorganized recommend and ensemble into classes.
    Added some tests with ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,18 +1,60 @@
 from bisect import insort
+from pmf import nise
+from dbread import read_ratings
+from recommend import Recommender
+from pmf import ProbabilisticMatrixFactorization as PMF
+#%%
+class Ensemble():
+    def __init__(self, MF_list):
+        self.MF_list = MF_list
+        self.RS = Recommender()
 
-def majority_ensemble (user_id, MF_list, topk):
-    n_items = MF_list[0][1].shape[0]
-    item_vote = [0]*n_items
-    for user_MF, item_MF in MF_list:
-        r_list = recommend(unrated_items, user_MF[user_id,], item_MF, topk)
-        r_list = [item for item, rating in r_list]
-        for item in r_list:
-            item_vote[item]+=1
-    ensemble = []
-    for item,votes in enumerate (item_vote):
-        insort(ensemble, (item,votes))
-    return ensemble[-topk:][::-1]
-    
+    def majority (self, user_vector,unrated_items, topk):
+        item_vote = [0]*self.MF_list[0].num_items
+        for pmf in self.MF_list:
+            self.RS.set_itemMF(pmf.items)
+            t_user_vector = self.RS.transform_user(user_vector)
+            r_list = self.RS.get_list(t_user_vector, unrated_items, topk)
+            print 'rlist', rlist
+            r_list = [item for item, rating in r_list]
+            for item in r_list:
+                item_vote[item]+=1
+        ensemble = []
+        for item,votes in enumerate (item_vote):
+            insort(ensemble, (item,votes))
+        return ensemble[-topk:][::-1]
 
-    
 
+
+if __name__ == "__main__":
+    #%%
+    DATASET = 'fake'
+
+    if DATASET == 'fake':
+        (ratings, u, i) = fake_ratings()
+        ratings = np.array(ratings).astype(float)
+        MF_list = []
+        for lambdar in [0.0,0.5,1.0]:
+            pmf = PMF(ratings, latent_d=5, regularization_strength=lambdar)
+            pmf.gradient_descent(ratings)
+            MF_list.append(pmf)
+    else:
+        filename = 'ml-100k/u.data'
+        ratings = read_ratings(filename)
+        ratings = np.array(ratings).astype(float)
+        MF_list = nise(ratings)
+
+    #%%
+    ens = Ensemble(MF_list)
+    uid=50.0
+    #gerar usuario teste
+    nitems = MF_list[0].num_items
+    user_vector = np.zeros((1,nitems),dtype=float)
+    for line in ratings:
+        if line[0]==uid:
+            user_vector[:,line[1]]=line[2]
+    unrated_items = [i for i,r in enumerate(user_vector) if r[0]==0.0]
+    topk = 5
+    #gera lista de rec.
+    rlist = ens.majority(user_vector,unrated_items,topk)
+    print 'final rlist',rlist
--- recommend.py
+++ recommend.py
@@ -1,26 +1,51 @@
 import numpy as np
 from bisect import insort
+#%%
+class Recommender():
 
-def recommend(unrated_items, user_MF_vector, item_MF, topk):
-    n_items = item_MF.shape[0]
-    new_ratings = []
-    for item in unrated_items:
-        r_hat = sum(user_MF_vector*(item_MF[item-1,:]))
-        insort(new_ratings, (r_hat, item))
-    recommended_list = [(item,rating) for rating, item in new_ratings[-topk:][::-1]]      
-    return recommended_list
-    
+    def __init__(self, item_MF=None):
+        self.item_MF = []
+        self.ITpinv = []
+        self.n_items = []
+        if item_MF != None:
+            self.set_itemMF(item_MF)
+        self.user_MF = []
+
+    def set_userMF (self, mat):
+        self.user_MF = mat
+
+    def set_itemMF (self, mat):
+        self.item_MF = mat
+        self.ITpinv = np.dot(mat,(np.linalg.inv(np.dot(mat.T,mat))))
+        self.n_items = mat.shape[0]
+
+
+    def get_user_vector(self, user_id):
+        return self.user_MF[user_id,:]
+
+    def transform_user(self, user_vector):
+        return np.dot(user_vector,self.ITpinv)
+
+    def get_list(self, user_MF_vector, unrated_items, topk):
+        new_ratings = []
+        for item in unrated_items:
+            r_hat = sum(user_MF_vector*(self.item_MF[item-1,:]))
+            insort(new_ratings, (r_hat, item))
+        recommended_list = [(item,rating) for rating, item in new_ratings[-topk:][::-1]]
+        return recommended_list
+#%%
 if __name__=='__main__':
     factors = 100
     nitems = 1000
     n_unrated = nitems-30
-    
+
     user_MF_vector = np.random.random(factors)
     item_MF = np.random.random((nitems, factors))
-    
+
     unrated_items = np.random.random_integers(1,nitems, n_unrated)
-    topk = 10  
-    
-    r_list = recommend(unrated_items, user_MF_vector, item_MF, topk)
-    
+    topk = 10
+    RecSys = Recommender(item_MF)
+
+    r_list = RecSys.get_list(user_MF_vector, unrated_items, topk)
+
     print r_list
