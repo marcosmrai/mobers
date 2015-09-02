86c5652a4dd1bfaf7958d2b5d8d1f66eb1be863d
Marcos Medeiros Raimundo <marcosmrai@gmail.com>
2015-09-02 02:24:49 -0300

    Ready to run version

--- pmf.py
+++ pmf.py
@@ -139,128 +139,6 @@ class mf():
         self.users = self.theta[:self.nU*self.d].reshape((self.nU,self.d))
         self.items = self.theta[self.nU*self.d:].reshape((self.nI,self.d))
 
-
-class ProbabilisticMatrixFactorization():
-
-    def __init__(self, ratings, nUsers, nItems, d=50,lambdaa=0.1):
-        self.d = d
-        self.learning_rate = .001
-        self.lambdaa = lambdaa
-
-        self.converged = False
-
-        self.ratings = ratings
-
-        self.num_users = nUsers
-        self.num_items = nItems
-        self.num_ratings = len(self.ratings)
-
-        self.users = numpy.ones((self.num_users, self.d))/numpy.sqrt(self.d)#*0.001
-        self.items = numpy.ones((self.num_items, self.d))/numpy.sqrt(self.d)#*0.001
-        self.training_time = 0
-
-        self.objs = self.objErrors(self.users,self.items)
-        self.obj = (1-self.lambdaa)*self.objs[0]+(self.lambdaa)*self.objs[1]
-
-
-    # return the regularization parameter
-    def getReg(self):
-        return self.lambdaa
-
-    # calculates the error of a rating
-    def sampleError(self,rating,users,items):
-        i,j,rat=rating
-        return (i,j,numpy.dot(users[i],items[j])-rat)
-
-    def updateErrorVec(self, users, items):
-        self.errorVec=[(i,j,numpy.dot(users[i],items[j])-rat) for i,j,rat in self.ratings]
-
-    # calculate the objective functions
-    def objErrors(self, users, items):
-        sq_error = sum([(rating-numpy.dot(users[i],items[j]))**2 for i, j, rating in self.ratings])/self.num_ratings
-        L2_norm = (numpy.sum(users**2)+numpy.sum(items**2))/(self.users.size+self.items.size)
-
-        return np.array([sq_error,L2_norm])
-
-    # calculate the scalarized error
-    def error(self, users, items):
-        # update the error vector
-        self.updateErrorVec(users,items)
-
-        # calculate the objective functions
-        [sq_error,L2_norm] = self.objErrors(users,items)
-
-        #return the scalarized objective error
-        return (1-self.lambdaa)*sq_error + self.lambdaa * L2_norm
-
-    # apply an update
-    def update(self):
-        self.updateErrorVec(self.users,self.items)
-
-        # inicializate the gradient with the regularization factor
-        grad_o = self.lambdaa*2*self.users/(self.users.size+self.items.size)
-        grad_d = self.lambdaa*2*self.items/(self.users.size+self.items.size)
-
-        # add to the gradient the error factor
-        for i, j, err_ in self.errorVec:
-            grad_o[i] += (1-self.lambdaa)*self.items[j] * (err_)#/self.num_ratings
-            grad_d[j] += (1-self.lambdaa)*self.users[i] * (err_)#/self.num_ratings
-
-        while (not self.converged):
-            #calculates the initial likelehood
-            initial_lik = self.error(self.users,self.items)
-
-            self.try_updates(grad_o, grad_d)
-
-            #calculates the updated likelehood
-            final_lik = self.error(self.new_users, self.new_items)
-
-            if final_lik < initial_lik:
-                self.apply_updates(grad_o, grad_d)
-                self.learning_rate *= 1.25
-
-                if initial_lik - final_lik < 10**-5:
-                    self.converged = True
-
-                break
-            else:
-                self.learning_rate *= .5
-                self.undo_updates()
-
-            if self.learning_rate < 1e-10:
-                self.converged = True
-
-        return self.converged
-
-
-    def apply_updates(self, grad_o, grad_d):
-        self.users = numpy.copy(self.new_users)
-        self.items = numpy.copy(self.new_items)
-
-
-    def try_updates(self, grad_o, grad_d):
-        alpha = self.learning_rate
-        lambd = self.lambdaa
-
-        self.new_users = self.users - alpha * ((1-lambd)*grad_o + lambd * self.users)
-        self.new_items = self.items - alpha * ((1-lambd)*grad_d + lambd * self.items)
-
-
-    def undo_updates(self):
-        # Don't need to do anything here
-        pass
-
-
-    def optimize(self, batchsize=None):
-        t0 = time()
-        converged = False
-        while not converged:
-            converged = self.update()
-
-        self.objs = self.objErrors(self.users,self.items)
-        self.obj = (1-self.lambdaa)*self.objs[0]+(self.lambdaa)*self.objs[1]
-        self.training_time = time() - t0
-
 class niseCand():
     def __init__(self, n0, n1,norm):
         self.n0 = n0
@@ -318,19 +196,19 @@ def nise(ratings,nUsers,nItems,nSol=50,hVError=0.001,d=100,tol=10^-2,batchsize=N
             sols+=1
     return out
 
-def plotPareto(list_,ratings):
+def plotPareto(list_,ratings,figpath):
     plotL=np.array([i.objs for i in list_])
     plt.plot(plotL[:,0],plotL[:,1],'ok')
     plt.xlabel('Squared error objective')
     plt.ylabel('Regularization objective (L2 norm)')
-    plt.savefig('pareto.jpg')
+    plt.savefig(figpath)
 
 def niseRun(poolpar):
     fold,d=poolpar
     train,trainU,trainI,valid,validU,validI,test,testU,testI=fold_load('ml-100k',fold)
     t0 = time()
     out=nise(train,trainU,trainI,d=d)
-    plotPareto(out,train)
+    plotPareto(out,train,'pareto.jpg')
     with open('models/u-100k-fold-d%d-' % d +str(fold)+'.out', 'wb') as handle:
         pickle.dump(out, handle)
     runtime = time()-t0
@@ -338,24 +216,8 @@ def niseRun(poolpar):
     with open('models/u-100k-fold-d%d-' % d +str(fold)+'runtime.out', 'wb') as handle:
         pickle.dump(runtime, handle)
 
-
-
 if __name__ == "__main__":
     poolList = [(fold,d) for fold in range(5) for d in [5,15,25]]
-    #p=multiprocessing.Pool(4)
-    #p.map(niseRun,poolList)
+    p=multiprocessing.Pool(4)
+    p.map(niseRun,poolList)
     niseRun(poolList[0])
-
-    #fold = 0
-    #d = 5
-    #train,trainU,trainI,valid,validU,validI,test,testU,testI=fold_load('ml-100k',fold)
-    #pmf1 =ProbabilisticMatrixFactorization(train,trainU,trainI,5,lambdaa=0.3)
-    #pmf1.optimize()
-    #print pmf1.obj,pmf1.objs,pmf1.training_time
-    #print numpy.sum(pmf1.users**2)/pmf1.users.size+numpy.sum(pmf1.items**2)/pmf1.items.size
-    #print (numpy.sum(pmf1.users**2)+numpy.sum(pmf1.items**2))/(pmf1.users.size+pmf1.items.size)
-    #pmf2 = mf(train,trainU,trainI,5,lambdaa=0.3)
-    #pmf2.optimize()
-    #print pmf2.obj,pmf2.objs,pmf2.training_time
-    #print numpy.sum(pmf2.users**2)/pmf2.users.size+numpy.sum(pmf2.items**2)/pmf2.items.size
-    #print (numpy.sum(pmf2.users**2)+numpy.sum(pmf2.items**2))/(pmf2.users.size+pmf2.items.size)
